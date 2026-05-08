5987b858eac585d218ca6907d2ecb091810435f3
Daniel Ventura de Almeida <danielvalmeida91@gmail.com>
2026-05-07 22:19:56 -0300

    feat: conectando as duas aplicações

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,29 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
-const PORT = 3000
+const PORT = 5000
 
 app.use(express.json())
+app.use(cors())
+
+const corsOptions = {
+    origin: "*",
+}
+
+app.use(cors(corsOptions))
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Hello World" })
 })
 
 app.get('/users', (req, res) => {
+    return res.status(400).json({ message: "Não habilitado!" })
     res.status(200).json({
         data: {
             infos: {
                 users: [
-                    { name: "Daniel", age: 34 }
+                    { name: "Daniel", age: 35 }
                 ]
             }
         }
